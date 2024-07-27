8199ef375baa236af244da6d24b0a9bb89faeb3e
kalyanreddyc <139508882+kalyanreddyc@users.noreply.github.com>
2024-07-28 03:50:54 +0530

    Update buildandPublish.groovy

--- vars/buildandPublish.groovy
+++ vars/buildandPublish.groovy
@@ -1,28 +1,29 @@
 // vars/buildandPublish.groovy
 def call(String phase = 'verify') {
-    if (phase == 'deploy' && (env.BUILD_TYPE == 'SNAPSHOT' || env.BUILD_TYPE == 'RELEASE')) {
-        mvn("clean deploy")
+    if (phase == 'deploy') {
+        if (env.BUILD_TYPE == 'RELEASE') {
+            // Set the version to a release version (removing -SNAPSHOT)
+            sh "mvn versions:set -DnewVersion=\$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | sed 's/-SNAPSHOT//')"
+            sh "mvn clean deploy -Prelease"
+        } else if (env.BUILD_TYPE == 'SNAPSHOT') {
+            sh "mvn clean deploy -Psnapshot"
+        }
     } else {
-        mvn("clean verify") // Default action is to build and verify
+        mvn("clean verify")
     }
 }
 
 def mvn(String command) {
-    if (env.BUILD_TYPE == 'SNAPSHOT') {
-        sh "mvn ${command} -Psnapshot"
-    } else if (env.BUILD_TYPE == 'RELEASE') {
-        sh "mvn ${command} -Prelease"
-    } else {
-        sh "mvn ${command}"
-    }
+    sh "mvn ${command}"
 }
 
-//***previous-script***//
-// def call() {
-//     if (env.BUILD_TYPE == 'SNAPSHOT' || env.BUILD_TYPE == 'RELEASE') {
-//         mvn "clean deploy"
+//**********working-script*******///
+// vars/buildandPublish.groovy
+// def call(String phase = 'verify') {
+//     if (phase == 'deploy' && (env.BUILD_TYPE == 'SNAPSHOT' || env.BUILD_TYPE == 'RELEASE')) {
+//         mvn("clean deploy")
 //     } else {
-//         mvn "clean verify"
+//         mvn("clean verify") // Default action is to build and verify
 //     }
 // }
 
@@ -33,5 +34,14 @@ def mvn(String command) {
 //         sh "mvn ${command} -Prelease"
 //     } else {
 //         sh "mvn ${command}"
+//     }
+// }
+
+//***previous-script***//
+// def call() {
+//     if (env.BUILD_TYPE == 'SNAPSHOT' || env.BUILD_TYPE == 'RELEASE') {
+//         mvn "clean deploy"
+//     } else {
+//         mvn "clean verify"
 //     }
 // }
