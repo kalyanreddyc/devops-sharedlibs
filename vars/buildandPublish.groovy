77bf89083d1a0f64ed6af268bba2658d330e80ea
kalyanreddyc <139508882+kalyanreddyc@users.noreply.github.com>
2024-07-28 03:13:53 +0530

    Update buildandPublish.groovy

--- vars/buildandPublish.groovy
+++ vars/buildandPublish.groovy
@@ -1,8 +1,9 @@
-def call() {
-    if (env.BUILD_TYPE == 'SNAPSHOT' || env.BUILD_TYPE == 'RELEASE') {
-        mvn "clean deploy"
+// vars/buildandPublish.groovy
+def call(String phase = 'build') {
+    if (phase == 'deploy' && (env.BUILD_TYPE == 'SNAPSHOT' || env.BUILD_TYPE == 'RELEASE')) {
+        mvn("clean deploy")
     } else {
-        mvn "clean verify"
+        mvn("clean verify") // Default action is to build and verify
     }
 }
 
@@ -15,3 +16,22 @@ def mvn(String command) {
         sh "mvn ${command}"
     }
 }
+
+//***previous-script***//
+// def call() {
+//     if (env.BUILD_TYPE == 'SNAPSHOT' || env.BUILD_TYPE == 'RELEASE') {
+//         mvn "clean deploy"
+//     } else {
+//         mvn "clean verify"
+//     }
+// }
+
+// def mvn(String command) {
+//     if (env.BUILD_TYPE == 'SNAPSHOT') {
+//         sh "mvn ${command} -Psnapshot"
+//     } else if (env.BUILD_TYPE == 'RELEASE') {
+//         sh "mvn ${command} -Prelease"
+//     } else {
+//         sh "mvn ${command}"
+//     }
+// }
