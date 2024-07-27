f5d30604a399900b84341857d7dda28016d87fc8
kalyanreddyc <139508882+kalyanreddyc@users.noreply.github.com>
2024-07-28 02:06:32 +0530

    Update sonarQulaityChecks.groovy

--- vars/sonarQulaityChecks.groovy
+++ vars/sonarQulaityChecks.groovy
@@ -1,6 +1,17 @@
-def call() {
-    if (env.BUILD_TYPE != 'RELEASE') {
-        // Integrate with SonarQube
-        sh "mvn sonar:sonar -Dsonar.qualitygate.wait=true"
+// def call() {
+//     if (env.BUILD_TYPE != 'RELEASE') {
+//         // Integrate with SonarQube
+//         sh "mvn sonar:sonar -Dsonar.qualitygate.wait=true"
+//     }
+// }
+// Assuming you have 'script' passed into your library function
+def call(script) {
+    script.withSonarQubeEnv('Your_SonarQube_Environment_Name') {
+        // Only perform SonarQube analysis for non-release builds
+        if (script.env.BUILD_TYPE != 'RELEASE') {
+            // Execute Maven SonarQube analysis and wait for the Quality Gate
+            script.sh "mvn clean verify sonar:sonar -Dsonar.qualitygate.wait=true"
+        }
     }
 }
+
