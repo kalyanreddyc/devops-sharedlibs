aacc2cab3583d81ea36a3f4cd09f356d7ef9e870
kalyanreddyc <139508882+kalyanreddyc@users.noreply.github.com>
2024-07-28 02:29:56 +0530

    Update sonarQualityChecks.groovy

--- vars/sonarQualityChecks.groovy
+++ vars/sonarQualityChecks.groovy
@@ -1,3 +1,10 @@
+def call(WorkflowScript script) {
+    script.withSonarQubeEnv('sonar') {
+        if (script.env.BUILD_TYPE != 'RELEASE') {
+            script.sh("mvn clean verify sonar:sonar -Dsonar.qualitygate.wait=true")
+        }
+    }
+}
 // def call() {
 //     if (env.BUILD_TYPE != 'RELEASE') {
 //         // Integrate with SonarQube
@@ -15,21 +22,22 @@
 //         }
 //     }
 // }
+//**********//
 // Include proper imports if needed
-import org.jenkinsci.plugins.workflow.steps.StepContext
+// import org.jenkinsci.plugins.workflow.steps.StepContext
 
-def call(StepContext context) {
-    context.withSonarQubeEnv('Your_SonarQube_Environment_Name') {
-        // Only perform SonarQube analysis for non-release builds
-        if (context.env.BUILD_TYPE != 'RELEASE') {
-            // Execute Maven SonarQube analysis and wait for the Quality Gate
-            context.sh "mvn clean verify sonar:sonar -Dsonar.qualitygate.wait=true"
+// def call(StepContext context) {
+//     context.withSonarQubeEnv('Your_SonarQube_Environment_Name') {
+//         // Only perform SonarQube analysis for non-release builds
+//         if (context.env.BUILD_TYPE != 'RELEASE') {
+//             // Execute Maven SonarQube analysis and wait for the Quality Gate
+//             context.sh "mvn clean verify sonar:sonar -Dsonar.qualitygate.wait=true"
             
-            // Now wait for the Quality Gate result and handle accordingly
-            def qg = context.waitForQualityGate()
-            if (qg.status != 'OK') {
-                context.error "Quality Gate failed: ${qg.status}"
-            }
-        }
-    }
-}
+//             // Now wait for the Quality Gate result and handle accordingly
+//             def qg = context.waitForQualityGate()
+//             if (qg.status != 'OK') {
+//                 context.error "Quality Gate failed: ${qg.status}"
+//             }
+//         }
+//     }
+// }
