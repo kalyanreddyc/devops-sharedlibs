3de2b65d220c6fa252f6c5e549580f496c05a8c3
kalyanreddyc <139508882+kalyanreddyc@users.noreply.github.com>
2024-07-28 02:35:06 +0530

    Update sonarQualityChecks.groovy

--- vars/sonarQualityChecks.groovy
+++ vars/sonarQualityChecks.groovy
@@ -1,10 +1,11 @@
-def call(WorkflowScript script) {
+def call(def script) {
     script.withSonarQubeEnv('sonar') {
         if (script.env.BUILD_TYPE != 'RELEASE') {
             script.sh("mvn clean verify sonar:sonar -Dsonar.qualitygate.wait=true")
         }
     }
 }
+
 //script.withSonarQubeEnv('Your_SonarQube_Environment_Name') : This is the name you've given your SonarQube configuration in Jenkins's global configuration. Replace it with the actual identifier you have configured.
 // def call() {
 //     if (env.BUILD_TYPE != 'RELEASE') {
